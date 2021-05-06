c235bb7e4b5e03b652a2026858928431689e9c6d
Konstantin S <HavenDV@users.noreply.github.com>
2021-05-06 22:04:34 +1000

    refactor: Moved ColorToBrushConverter to shared project.

---
+++ src/shared/Ratbuddyssey.Shared/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+#nullable enable
+
+namespace Ratbuddyssey.Converters
+{
+    public class ColorToBrushConverter : BaseConverter<System.Drawing.Color, Brush>
+    {
+        public ColorToBrushConverter() : base(static color => new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B)))
+        {
+        }
+    }
+}
