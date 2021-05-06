98c1496416a5debabf85b2d7e710b55b031e5abc
Konstantin S <HavenDV@users.noreply.github.com>
2021-05-06 16:02:04 +1000

    feat(WPF): Added ColorToBrushConverter.

---
+++ src/apps/Ratbuddyssey/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Ratbuddyssey.Converters
+{
+    public class ColorToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not System.Drawing.Color color)
+            {
+                throw new NotImplementedException();
+            }
+
+            return new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
