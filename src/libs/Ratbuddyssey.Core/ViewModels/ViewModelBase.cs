91a422661aa81ca380580d55d72908dfd6606d85
Konstantin S <HavenDV@users.noreply.github.com>
2021-05-07 16:18:34 +1000

    refactor(Core): Removed unused code.

--- src/libs/Ratbuddyssey.Core/ViewModels/ViewModelBase.cs
+++ src/libs/Ratbuddyssey.Core/ViewModels/ViewModelBase.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
 
 namespace Ratbuddyssey.ViewModels
 {
     public abstract class ViewModelBase : ReactiveObject
     {
-    }
-
-    public abstract class SelectableViewModel : ReactiveObject
-    {
-        [Reactive]
-        public bool IsSelected { get; set; }
     }
 
     public abstract class ActivatableViewModel : ViewModelBase, IActivatableViewModel
@@ -30,11 +23,6 @@ namespace Ratbuddyssey.ViewModels
         public string? UrlPathSegment { get; set; }
         public IScreen HostScreen { get; }
 
-        public ReactiveCommand<Type, IRoutableViewModel> NavigateByType { get; } = ReactiveCommand.Create<Type, IRoutableViewModel>(
-            static type => throw new InvalidOperationException("HostScreen is not NavigationViewModel."));
-        public ReactiveCommand<IRoutableViewModel, IRoutableViewModel> NavigateByViewModel { get; } = ReactiveCommand.Create<IRoutableViewModel, IRoutableViewModel>(
-            static type => throw new InvalidOperationException("HostScreen is not NavigationViewModel."));
-
         #endregion
 
         #region Constructors
@@ -43,12 +31,6 @@ namespace Ratbuddyssey.ViewModels
         {
             HostScreen = hostScreen ?? throw new ArgumentNullException(nameof(hostScreen));
             UrlPathSegment = name;
-
-            //if (HostScreen is NavigationViewModel navigationViewModel)
-            //{
-            //    NavigateByType = navigationViewModel.Navigate;
-            //    NavigateByViewModel = navigationViewModel.Router.Navigate;
-            //}
         }
 
         #endregion
