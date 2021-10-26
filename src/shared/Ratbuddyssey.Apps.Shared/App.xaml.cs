f5ac2c9172c17343c94f28ba583227151ab7a368
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-26 19:07:42 +1000

    refactor: To common App.xaml.cs.

---
+++ src/shared/Ratbuddyssey.Apps.Shared/App.xaml.cs
@@ -0,0 +1,132 @@
+﻿using ReactiveUI;
+using H.ReactiveUI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ratbuddyssey.Initialization;
+using Ratbuddyssey.ViewModels;
+#if WPF_APP
+using System.Windows;
+#else
+using System;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml.Controls;
+#endif
+
+#nullable enable
+
+namespace Ratbuddyssey;
+
+public sealed partial class App
+{
+    #region Properties
+
+    public IHost Host { get; }
+    private InteractionManager InteractionManager { get; } = new();
+
+    #endregion
+
+    #region Constructors
+
+    public App()
+    {
+        InteractionManager.Register();
+
+#if WPF_APP
+        DispatcherUnhandledException += static (_, e) =>
+        {
+            e.Handled = true;
+
+            MessageInteractions.Exception.Handle(e.Exception).Subscribe();
+        };
+#else
+        UnhandledException += static (sender, args) =>
+        {
+            args.Handled = true;
+
+            _ = MessageInteractions.Exception
+                .Handle(args.Exception)
+                .Subscribe();
+        };
+#endif
+
+        Host = Initialization.HostBuilder
+            .Create()
+            .AddViews()
+            .AddConverters()
+            .AddPlatformSpecificLoggers()
+#if __WASM__
+            .RemoveFileWatchers()
+#endif
+            .Build();
+
+#if !WPF_APP
+        InitializeComponent();
+#endif
+    }
+
+    #endregion
+
+    #region Event Handlers
+
+    private IViewFor GetView<T>(out T viewModel) where T : notnull
+    {
+        viewModel = Host.Services.GetRequiredService<T>();
+        var view = Host.Services
+            .GetRequiredService<IViewLocator>()
+            .ResolveView(viewModel) ??
+            throw new InvalidOperationException("View is null.");
+
+        view.ViewModel = viewModel;
+        return view;
+    }
+
+#if WPF_APP
+
+    private void Application_Startup(object _, StartupEventArgs e)
+    {
+        var mainView = (Window)GetView<MainViewModel>(out var _);
+
+        if (e.Args.Any())
+        {
+            var path = e.Args.ElementAtOrDefault(0);
+            if (path != null && !string.IsNullOrWhiteSpace(path))
+            {
+                var fileViewModel = Host.Services.GetRequiredService<FileViewModel>();
+
+                fileViewModel.Open(path.ToFile());
+            }
+        }
+
+        mainView.ShowDialog();
+    }
+
+#else
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        // Uno.iOS requires full namespace.
+        var window = Windows.UI.Xaml.Window.Current;
+        if (window.Content is not Frame frame)
+        {
+            frame = new Frame();
+
+            window.Content = frame;
+        }
+
+        if (args.PrelaunchActivated)
+        {
+            return;
+        }
+
+        if (frame.Content is null)
+        {
+            frame.Content = GetView<MainViewModel>(out var _);
+        }
+
+        window.Activate();
+    }
+
+#endif
+
+    #endregion
+}
