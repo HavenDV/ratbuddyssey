a6b97fb2ddc2b19af76ee61682f335739db3e024
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-29 02:46:27 +1000

    refactor: Converter MainView to xaml bindings.

--- src/shared/Ratbuddyssey.Apps.Shared/Views/MainView.xaml.cs
+++ src/shared/Ratbuddyssey.Apps.Shared/Views/MainView.xaml.cs
@@ -1,29 +1,7 @@
 ﻿#nullable enable
 
-namespace Ratbuddyssey.Views
+namespace Ratbuddyssey.Views;
+
+public partial class MainView
 {
-    public partial class MainView
-    {
-        #region Constructors
-
-        public MainView()
-        {
-            InitializeComponent();
-
-            _ = this.WhenActivated(disposable =>
-            {
-                if (ViewModel == null)
-                {
-                    return;
-                }
-
-                _ = this.OneWayBind(ViewModel,
-                        static viewModel => viewModel.Router,
-                        static view => view.RoutedViewHost.Router)
-                    .DisposeWith(disposable);
-            });
-        }
-
-        #endregion
-    }
 }
