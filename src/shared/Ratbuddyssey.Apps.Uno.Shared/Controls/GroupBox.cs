58697c2ac658d0c4916b2134b98f8ca259c4a0f6
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-30 15:52:05 +1000

    feat: Added GroupBox.

---
+++ src/shared/Ratbuddyssey.Apps.Uno.Shared/Controls/GroupBox.cs
@@ -0,0 +1,24 @@
+﻿namespace Ratbuddyssey.Controls;
+
+internal class GroupBox : ContentControl
+{
+    #region Dependency Properties
+
+    #region Header
+
+    public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register(
+        nameof(Header),
+        typeof(string),
+        typeof(GroupBox),
+        new PropertyMetadata(string.Empty));
+    
+    public string? Header
+    {
+        get => (string?)GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+
+    #endregion
+
+    #endregion
+}
