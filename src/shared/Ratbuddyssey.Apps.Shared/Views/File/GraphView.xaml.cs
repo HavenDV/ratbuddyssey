1f55e17b0643c744154f03e2766647c94a9bef9d
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-31 03:12:53 +1000

    refactor: To generated GraphView.xaml.cs.

--- src/shared/Ratbuddyssey.Apps.Shared/Views/File/GraphView.xaml.cs
+++ src/shared/Ratbuddyssey.Apps.Shared/Views/File/GraphView.xaml.cs
@@ -1,56 +1,11 @@
-﻿#if HAS_WPF
-using System.Reactive.Disposables;
-using ReactiveUI;
+﻿namespace Ratbuddyssey.Views;
 
-#nullable enable
-
-namespace Ratbuddyssey.Views
+public partial class GraphView
 {
-    public partial class GraphView
+#if HAS_WPF
+    partial void AfterInitializeComponent()
     {
-        #region Constructors
-
-        public GraphView()
-        {
-            InitializeComponent();
-
-            PlotView.PreviewMouseWheel += (_, args) => args.Handled = true;
-
-            _ = this.WhenActivated(disposable =>
-            {
-                if (ViewModel == null)
-                {
-                    return;
-                }
-
-                _ = this.OneWayBind(ViewModel,
-                        static viewModel => viewModel.MeasurementPositions,
-                        static view => view.MeasurementPositionsListView.ItemsSource)
-                    .DisposeWith(disposable);
-                _ = this.Bind(ViewModel,
-                        static viewModel => viewModel.SelectAllMeasurementPositionsIsChecked,
-                        static view => view.SelectAllMeasurementPositionsCheckBox.IsChecked)
-                    .DisposeWith(disposable);
-                _ = this.OneWayBind(ViewModel,
-                        static viewModel => viewModel.PlotModel,
-                        static view => view.PlotView.Model)
-                    .DisposeWith(disposable);
-                _ = this.OneWayBind(ViewModel,
-                        static viewModel => viewModel.SmoothingFactors,
-                        static view => view.SmoothingFactorsListView.ItemsSource)
-                    .DisposeWith(disposable);
-                _ = this.OneWayBind(ViewModel,
-                        static viewModel => viewModel.Ranges,
-                        static view => view.RangesListView.ItemsSource)
-                    .DisposeWith(disposable);
-                _ = this.Bind(ViewModel,
-                        static viewModel => viewModel.LogarithmicAxisIsChecked,
-                        static view => view.LogarithmicAxisCheckBox.IsChecked)
-                    .DisposeWith(disposable);
-            });
-        }
-
-        #endregion
+        PlotView.PreviewMouseWheel += (_, args) => args.Handled = true;
     }
-}
 #endif
+}
