c4daf8e5eb037be27f8fefb959aa58159d591695
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-31 01:48:45 +1000

    fix: Fixed WebAssembly bug.

--- src/shared/Ratbuddyssey.Apps.Shared/Views/File/FileView.xaml.cs
+++ src/shared/Ratbuddyssey.Apps.Shared/Views/File/FileView.xaml.cs
@@ -2,4 +2,38 @@
 
 public partial class FileView
 {
+    // Workaround for issue: 
+    partial void AfterWhenActivated(CompositeDisposable disposables)
+    {
+        _ = this.OneWayBind(ViewModel,
+                static viewModel => viewModel.StatusViewModel,
+                static view => view.StatusView.ViewModel)
+            .DisposeWith(disposables);
+        _ = this.OneWayBind(ViewModel,
+                static viewModel => viewModel.ChannelsViewModel,
+                static view => view.ChannelsView.ViewModel)
+            .DisposeWith(disposables);
+#if HAS_WPF
+        _ = this.OneWayBind(ViewModel,
+                static viewModel => viewModel.TargetCurvePointsViewModel,
+                static view => view.TargetCurvePointsView.ViewModel)
+            .DisposeWith(disposables);
+        _ = this.OneWayBind(ViewModel,
+                static viewModel => viewModel.ChannelInformationViewModel,
+                static view => view.ChannelInformationView.ViewModel)
+            .DisposeWith(disposables);
+        _ = this.OneWayBind(ViewModel,
+                static viewModel => viewModel.ChannelReportViewModel,
+                static view => view.ChannelReportView.ViewModel)
+            .DisposeWith(disposables);
+        _ = this.OneWayBind(ViewModel,
+                static viewModel => viewModel.GraphViewModel,
+                static view => view.GraphView.ViewModel)
+            .DisposeWith(disposables);
+#endif
+        _ = this.OneWayBind(ViewModel,
+                static viewModel => viewModel.PreviewDropViewModel,
+                static view => view.PreviewDropView.ViewModel)
+            .DisposeWith(disposables);
+    }
 }
