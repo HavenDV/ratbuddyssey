f007ca761cc3bcfedd9c7e78a1daa8e9b224dc82
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-31 00:57:31 +1000

    refactor: To generated PreviewDropView.

--- src/shared/Ratbuddyssey.Apps.Shared/Views/File/PreviewDropView.xaml.cs
+++ src/shared/Ratbuddyssey.Apps.Shared/Views/File/PreviewDropView.xaml.cs
@@ -1,28 +1,5 @@
-﻿#nullable enable
-
-namespace Ratbuddyssey.Views;
+﻿namespace Ratbuddyssey.Views;
 
 public partial class PreviewDropView
 {
-    #region Constructors
-
-    public PreviewDropView()
-    {
-        InitializeComponent();
-
-        _ = this.WhenActivated(disposable =>
-        {
-            if (ViewModel == null)
-            {
-                return;
-            }
-
-            _ = this.OneWayBind(ViewModel,
-                    static viewModel => viewModel.Names,
-                    static view => view.NamesListView.ItemsSource)
-                .DisposeWith(disposable);
-        });
-    }
-
-    #endregion
 }
