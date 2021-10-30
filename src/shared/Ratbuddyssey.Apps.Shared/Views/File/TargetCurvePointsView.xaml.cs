0e3a22e8997e998314e42e9f68f2eeb615691f38
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-31 02:37:40 +1000

    refactor: Changed TargetCurvePointsView to generated .xaml.cs.

--- src/shared/Ratbuddyssey.Apps.Shared/Views/File/TargetCurvePointsView.xaml.cs
+++ src/shared/Ratbuddyssey.Apps.Shared/Views/File/TargetCurvePointsView.xaml.cs
@@ -1,71 +1,51 @@
 ﻿#if HAS_WPF
-using System;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Windows.Controls;
 using Ratbuddyssey.ViewModels;
-using ReactiveUI;
+#endif
 
 #nullable enable
 
-namespace Ratbuddyssey.Views
+namespace Ratbuddyssey.Views;
+
+public partial class TargetCurvePointsView
 {
-    public partial class TargetCurvePointsView
+#if HAS_WPF
+    partial void AfterWhenActivated(CompositeDisposable disposables)
     {
-        #region Constructors
-
-        public TargetCurvePointsView()
+        if (ViewModel == null)
         {
-            InitializeComponent();
+            return;
+        }
 
-            _ = this.WhenActivated(disposable =>
+
+        _ = Observable.FromEventPattern<InitializingNewItemEventArgs>(
+                       TargetCurvePointsDataGrid,
+                       nameof(TargetCurvePointsDataGrid.InitializingNewItem))
+                   .Subscribe(value =>
+                   {
+                       if (value.EventArgs.NewItem is not TargetCurvePointViewModel point)
+                       {
+                           return;
+                       }
+
+                       _ = point.Delete
+                           .InvokeCommand(ViewModel.DeleteTargetCurvePoint);
+                   })
+                   .DisposeWith(disposables);
+        _ = Observable.FromEventPattern<DataGridRowEditEndingEventArgs>(
+                TargetCurvePointsDataGrid,
+                nameof(TargetCurvePointsDataGrid.RowEditEnding))
+            .Subscribe(static value =>
             {
-                if (ViewModel == null)
+                if (!value.EventArgs.Row.IsNewItem ||
+                    value.EventArgs.Row.Item is not TargetCurvePointViewModel point)
                 {
                     return;
                 }
 
-                // Target Curve Points.
-                _ = this.OneWayBind(ViewModel,
-                        static viewModel => viewModel.IsChannelSelected,
-                        static view => view.TargetCurvePointsGroupBox.IsEnabled)
-                    .DisposeWith(disposable);
-                _ = this.OneWayBind(ViewModel,
-                        static viewModel => viewModel.CustomTargetCurvePoints,
-                        static view => view.TargetCurvePointsDataGrid.ItemsSource)
-                    .DisposeWith(disposable);
-                _ = Observable.FromEventPattern<InitializingNewItemEventArgs>(
-                        TargetCurvePointsDataGrid,
-                        nameof(TargetCurvePointsDataGrid.InitializingNewItem))
-                    .Subscribe(value =>
-                    {
-                        if (value.EventArgs.NewItem is not TargetCurvePointViewModel point)
-                        {
-                            return;
-                        }
-
-                        _ = point.Delete
-                            .InvokeCommand(ViewModel.DeleteTargetCurvePoint);
-                    })
-                    .DisposeWith(disposable);
-                _ = Observable.FromEventPattern<DataGridRowEditEndingEventArgs>(
-                        TargetCurvePointsDataGrid,
-                        nameof(TargetCurvePointsDataGrid.RowEditEnding))
-                    .Subscribe(static value =>
-                    {
-                        if (!value.EventArgs.Row.IsNewItem ||
-                            value.EventArgs.Row.Item is not TargetCurvePointViewModel point)
-                        {
-                            return;
-                        }
-
-                        point.IsNew = false;
-                    })
-                    .DisposeWith(disposable);
-            });
-        }
-
-        #endregion
+                point.IsNew = false;
+            })
+            .DisposeWith(disposables);
     }
-}
 #endif
+}
