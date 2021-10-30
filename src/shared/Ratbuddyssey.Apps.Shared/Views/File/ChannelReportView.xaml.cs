6826415c2e749d87280bc3f07586b20288580f7e
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-10-31 02:52:42 +1000

    refactor: To generated ChannelReportView.xaml.cs.

--- src/shared/Ratbuddyssey.Apps.Shared/Views/File/ChannelReportView.xaml.cs
+++ src/shared/Ratbuddyssey.Apps.Shared/Views/File/ChannelReportView.xaml.cs
@@ -1,50 +1,5 @@
-﻿#if HAS_WPF
-using System.Reactive.Disposables;
-using ReactiveUI;
+﻿namespace Ratbuddyssey.Views;
 
-#nullable enable
-
-namespace Ratbuddyssey.Views
+public partial class ChannelReportView
 {
-    public partial class ChannelReportView
-    {
-        #region Constructors
-
-        public ChannelReportView()
-        {
-            InitializeComponent();
-
-            _ = this.WhenActivated(disposable =>
-            {
-                if (ViewModel == null)
-                {
-                    return;
-                }
-
-                _ = this.OneWayBind(ViewModel,
-                        static viewModel => viewModel.IsChannelSelected,
-                        static view => view.ChannelReportGroupBox.IsEnabled)
-                    .DisposeWith(disposable);
-                _ = this.Bind(ViewModel,
-                        static viewModel => viewModel.Channel.Data.ChannelReport.EnSpeakerConnect,
-                        static view => view.EnSpeakerConnectTextBox.Text)
-                    .DisposeWith(disposable);
-                _ = this.Bind(ViewModel,
-                        static viewModel => viewModel.Channel.Data.ChannelReport.CustomEnSpeakerConnect,
-                        static view => view.CustomEnSpeakerConnectTextBox.Text)
-                    .DisposeWith(disposable);
-                _ = this.Bind(ViewModel,
-                        static viewModel => viewModel.Channel.Data.ChannelReport.Distance,
-                        static view => view.DistanceTextBox.Text)
-                    .DisposeWith(disposable);
-                _ = this.Bind(ViewModel,
-                        static viewModel => viewModel.Channel.Data.ChannelReport.IsReversePolarity,
-                        static view => view.IsReversePolarityCheckBox.IsChecked)
-                    .DisposeWith(disposable);
-            });
-        }
-
-        #endregion
-    }
 }
-#endif
