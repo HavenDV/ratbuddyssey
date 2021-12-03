5906a73ea74ba74f54db99125e345c4a2bf669bf
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-12-04 08:34:43 +1000

    feat: Added GridSplitter.

---
+++ src/shared/Ratbuddyssey.Apps.Uno.Shared/Controls/GridSplitter.cs
@@ -0,0 +1,9 @@
+﻿namespace Ratbuddyssey.Controls;
+
+#if HAS_WINUI
+public class GridSplitter : CommunityToolkit.WinUI.UI.Controls.GridSplitter
+#else
+public class GridSplitter : Microsoft.Toolkit.Uwp.UI.Controls.GridSplitter
+#endif
+{
+}
