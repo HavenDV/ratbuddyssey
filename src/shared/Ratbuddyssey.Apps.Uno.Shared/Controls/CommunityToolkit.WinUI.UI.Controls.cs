0d0a44d9c66cd2f05f1baf1694d168efc0d2abce
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-12-04 13:39:01 +1000

    refactor: To CommunityToolkit.WinUI.UI.Controls.cs file.

---
+++ src/shared/Ratbuddyssey.Apps.Uno.Shared/Controls/CommunityToolkit.WinUI.UI.Controls.cs
@@ -0,0 +1,7 @@
+﻿#if !HAS_WINUI
+namespace CommunityToolkit.WinUI.UI.Controls;
+
+public class GridSplitter : Microsoft.Toolkit.Uwp.UI.Controls.GridSplitter
+{
+}
+#endif
--- src/shared/Ratbuddyssey.Apps.Uno.Shared/Controls/GridSplitter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ratbuddyssey.Controls;
-
-#if HAS_WINUI
-public class GridSplitter : CommunityToolkit.WinUI.UI.Controls.GridSplitter
-#else
-public class GridSplitter : Microsoft.Toolkit.Uwp.UI.Controls.GridSplitter
-#endif
-{
-}
