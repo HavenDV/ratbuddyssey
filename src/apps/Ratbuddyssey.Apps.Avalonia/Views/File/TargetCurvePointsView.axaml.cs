b70da44e435ca52dc534069d64e247c9ce314a98
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2021-12-12 15:53:02 +1000

    feat(Avalonia): Implemented TargetCurvePointsView.

---
+++ src/apps/Ratbuddyssey.Apps.Avalonia/Views/File/TargetCurvePointsView.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Ratbuddyssey.Apps.Views;
+
+public partial class TargetCurvePointsView : UserControl
+{
+    public TargetCurvePointsView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
